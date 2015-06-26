a3f6d3a0b0af17fbde71e0602ff7719dc4021501
Luis  Alberto Carvajal Martinez <luisalbcarvajal@gmail.com>
2015-06-26 16:56:57 -0500

    createDB and .gitignore updated

--- RulesEngine/gist/CreateDB.sql
+++ RulesEngine/gist/CreateDB.sql
@@ -1,5 +1,3 @@
-
-/****** Object:  Database [WSO_RulesEngine]    Script Date: 26/06/2015 11:57:35 a. m. ******/
 CREATE DATABASE [RulesEngine]
 GO
 USE RulesEngine
@@ -77,6 +75,80 @@ CREATE TYPE [dbo].[RulesTableType] AS TABLE(
 	[RuleConfiguration] [xml] NOT NULL
 )
 GO
+
+CREATE TABLE [dbo].[RuleType](
+	[RuleTypeID] [int] IDENTITY(1,1) NOT NULL  CONSTRAINT [PK_RuleType] PRIMARY KEY CLUSTERED,
+	[Name] [varchar](50) NOT NULL,
+	[CreateByUserID] [int] NOT NULL,
+	[CreateDate] [datetime] NOT NULL CONSTRAINT [DF_RuleType_CreateDate]  DEFAULT (getutcdate()),
+	[Enabled] [bit] NOT NULL CONSTRAINT [DF_RuleType_Enabled]  DEFAULT ((1)),
+	[ModifyByUserID] [int] NULL,
+	[ModifyDate] [datetime] NULL)
+GO
+
+CREATE TABLE [dbo].[RuleProfile](
+	[RuleProfileID] [int] IDENTITY(1,1) NOT NULL CONSTRAINT [PK_Profile] PRIMARY KEY CLUSTERED,
+	[Name] [varchar](50) NOT NULL,
+	[CreateByUserID] [int] NOT NULL,
+	[CreateDate] [datetime] NOT NULL CONSTRAINT [DF_RulesProfile_CreateDate]  DEFAULT (getutcdate()),
+	[Enabled] [bit] NOT NULL CONSTRAINT [DF_RulesProfile_Enabled]  DEFAULT ((1)),
+	[ModifyByUserID] [int] NULL,
+	[ModifyDate] [datetime] NULL)
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [IDX_RuleProfile_Name_Enabled] ON [dbo].[RuleProfile]
+(
+	[Name] ASC,
+	[Enabled] ASC
+)
+WHERE ([Enabled]=(1))
+GO
+
+CREATE TABLE [dbo].[RuleGroup](
+	[RuleGroupID] [int] IDENTITY(1,1) NOT NULL  CONSTRAINT [PK_Group] PRIMARY KEY CLUSTERED,
+	[RuleProfileID] [int] NOT NULL CONSTRAINT [FK_Group_Profile] FOREIGN KEY REFERENCES [dbo].[RuleProfile] ([RuleProfileID]),
+	[IsSystem] [bit] NOT NULL,
+	[CreateByUserID] [int] NOT NULL,
+	[CreateDate] [datetime] NOT NULL CONSTRAINT [DF_RulesGroup_CreateDate]  DEFAULT (getutcdate()),
+	[Enabled] [bit] NOT NULL CONSTRAINT [DF_RulesGroup_Enabled]  DEFAULT ((1)),
+	[ModifyByUserID] [int] NULL,
+	[ModifyDate] [datetime] NULL)
+GO
+
+CREATE TABLE [dbo].[RuleDetail](
+	[RuleDetailID] [int] IDENTITY(1,1) NOT NULL CONSTRAINT [PK_RuleDetail] PRIMARY KEY CLUSTERED ,
+	[RuleTypeID] [int] NOT NULL  CONSTRAINT [FK_RuleDetail_RuleType] FOREIGN KEY([RuleTypeID]) REFERENCES [dbo].[RuleType] ([RuleTypeID]),
+	[RuleGroupID] [int] NOT NULL CONSTRAINT [FK_Rule_Group] FOREIGN KEY REFERENCES [dbo].[RuleGroup] ([RuleGroupID]),
+	[RuleConfiguration] [xml](CONTENT [dbo].[RuleType]) NOT NULL,
+	[CreateByUserID] [int] NOT NULL,
+	[CreateDate] [datetime] NOT NULL CONSTRAINT [DF_RuleDetail_CreateDate]  DEFAULT (getutcdate()),
+	[Enabled] [bit] NOT NULL CONSTRAINT [DF_RuleDetail_Enabled]  DEFAULT ((1)),
+	[ModifyByUserID] [int] NULL,
+	[ModifyDate] [datetime] NULL)
+GO
+
+CREATE UNIQUE NONCLUSTERED INDEX [IDX_RuleDetail_RuleTypeID_RuleGroupID] ON [dbo].[RuleDetail]
+(
+	[RuleGroupID] ASC,
+	[RuleTypeID] ASC
+)
+  
+                                    
+SET IDENTITY_INSERT [dbo].[RuleType] ON 
+
+GO
+INSERT [dbo].[RuleType] ([RuleTypeID], [Name], [CreateByUserID], [CreateDate], [Enabled], [ModifyByUserID], [ModifyDate]) VALUES (1, N'Payment Method', 1, CAST(N'2015-06-25 23:15:50.140' AS DateTime), 1, NULL, NULL)
+GO
+INSERT [dbo].[RuleType] ([RuleTypeID], [Name], [CreateByUserID], [CreateDate], [Enabled], [ModifyByUserID], [ModifyDate]) VALUES (2, N'Region', 1, CAST(N'2015-06-25 23:15:50.140' AS DateTime), 1, NULL, NULL)
+GO
+INSERT [dbo].[RuleType] ([RuleTypeID], [Name], [CreateByUserID], [CreateDate], [Enabled], [ModifyByUserID], [ModifyDate]) VALUES (3, N'Quantity', 1, CAST(N'2015-06-25 23:15:50.140' AS DateTime), 1, NULL, NULL)
+GO
+INSERT [dbo].[RuleType] ([RuleTypeID], [Name], [CreateByUserID], [CreateDate], [Enabled], [ModifyByUserID], [ModifyDate]) VALUES (4, N'Odometer', 1, CAST(N'2015-06-25 23:15:50.140' AS DateTime), 1, NULL, NULL)
+GO
+SET IDENTITY_INSERT [dbo].[RuleType] OFF
+GO
+
+
+
 /****** Object:  StoredProcedure [dbo].[CreateProfile]    Script Date: 26/06/2015 11:57:35 a. m. ******/
 
 CREATE PROCEDURE [dbo].[CreateProfile] (
@@ -194,78 +266,4 @@ AS BEGIN
 	SELECT @errorMessage = ERROR_MESSAGE(),	@errorSeverity = ERROR_SEVERITY()
 	RAISERROR(@errorMessage, @errorSeverity, 1)
 END
-GO
-/****** Object:  Table [dbo].[RuleDetail]    Script Date: 26/06/2015 11:57:35 a. m. ******/
-
-CREATE TABLE [dbo].[RuleDetail](
-	[RuleDetailID] [int] IDENTITY(1,1) NOT NULL CONSTRAINT [PK_RuleDetail] PRIMARY KEY CLUSTERED ,
-	[RuleTypeID] [int] NOT NULL  CONSTRAINT [FK_RuleDetail_RuleType] FOREIGN KEY([RuleTypeID]) REFERENCES [dbo].[RuleType] ([RuleTypeID]),
-	[RuleGroupID] [int] NOT NULL CONSTRAINT [FK_Rule_Group] FOREIGN KEY REFERENCES [dbo].[RuleGroup] ([RuleGroupID]),
-	[RuleConfiguration] [xml](CONTENT [dbo].[RuleType]) NOT NULL,
-	[CreateByUserID] [int] NOT NULL,
-	[CreateDate] [datetime] NOT NULL CONSTRAINT [DF_RuleDetail_CreateDate]  DEFAULT (getutcdate()),
-	[Enabled] [bit] NOT NULL CONSTRAINT [DF_RuleDetail_Enabled]  DEFAULT ((1)),
-	[ModifyByUserID] [int] NULL,
-	[ModifyDate] [datetime] NULL)
-GO
-CREATE UNIQUE NONCLUSTERED INDEX [IDX_RuleDetail_RuleTypeID_RuleGroupID] ON [dbo].[RuleDetail]
-(
-	[RuleGroupID] ASC,
-	[RuleTypeID] ASC
-)
-GO
-/****** Object:  Table [dbo].[RuleGroup]    Script Date: 26/06/2015 11:57:35 a. m. ******/
-
-CREATE TABLE [dbo].[RuleGroup](
-	[RuleGroupID] [int] IDENTITY(1,1) NOT NULL  CONSTRAINT [PK_Group] PRIMARY KEY CLUSTERED,
-	[RuleProfileID] [int] NOT NULL CONSTRAINT [FK_Group_Profile] FOREIGN KEY REFERENCES [dbo].[RuleProfile] ([RuleProfileID]),
-	[IsSystem] [bit] NOT NULL,
-	[CreateByUserID] [int] NOT NULL,
-	[CreateDate] [datetime] NOT NULL CONSTRAINT [DF_RulesGroup_CreateDate]  DEFAULT (getutcdate()),
-	[Enabled] [bit] NOT NULL CONSTRAINT [DF_RulesGroup_Enabled]  DEFAULT ((1)),
-	[ModifyByUserID] [int] NULL,
-	[ModifyDate] [datetime] NULL)
-GO
-/****** Object:  Table [dbo].[RuleProfile]    Script Date: 26/06/2015 11:57:35 a. m. ******/
-
-CREATE TABLE [dbo].[RuleProfile](
-	[RuleProfileID] [int] IDENTITY(1,1) NOT NULL CONSTRAINT [PK_Profile] PRIMARY KEY CLUSTERED,
-	[Name] [varchar](50) NOT NULL,
-	[CreateByUserID] [int] NOT NULL,
-	[CreateDate] [datetime] NOT NULL CONSTRAINT [DF_RulesProfile_CreateDate]  DEFAULT (getutcdate()),
-	[Enabled] [bit] NOT NULL CONSTRAINT [DF_RulesProfile_Enabled]  DEFAULT ((1)),
-	[ModifyByUserID] [int] NULL,
-	[ModifyDate] [datetime] NULL)
-GO
-CREATE UNIQUE NONCLUSTERED INDEX [IDX_RuleProfile_Name_Enabled] ON [dbo].[RuleProfile]
-(
-	[Name] ASC,
-	[Enabled] ASC
-)
-WHERE ([Enabled]=(1))
-GO
-/****** Object:  Table [dbo].[RuleType]    Script Date: 26/06/2015 11:57:35 a. m. ******/
-
-CREATE TABLE [dbo].[RuleType](
-	[RuleTypeID] [int] IDENTITY(1,1) NOT NULL  CONSTRAINT [PK_RuleType] PRIMARY KEY CLUSTERED,
-	[Name] [varchar](50) NOT NULL,
-	[CreateByUserID] [int] NOT NULL,
-	[CreateDate] [datetime] NOT NULL CONSTRAINT [DF_RuleType_CreateDate]  DEFAULT (getutcdate()),
-	[Enabled] [bit] NOT NULL CONSTRAINT [DF_RuleType_Enabled]  DEFAULT ((1)),
-	[ModifyByUserID] [int] NULL,
-	[ModifyDate] [datetime] NULL)
-GO
-                                    
-SET IDENTITY_INSERT [dbo].[RuleType] ON 
-
-GO
-INSERT [dbo].[RuleType] ([RuleTypeID], [Name], [CreateByUserID], [CreateDate], [Enabled], [ModifyByUserID], [ModifyDate]) VALUES (1, N'Payment Method', 1, CAST(N'2015-06-25 23:15:50.140' AS DateTime), 1, NULL, NULL)
-GO
-INSERT [dbo].[RuleType] ([RuleTypeID], [Name], [CreateByUserID], [CreateDate], [Enabled], [ModifyByUserID], [ModifyDate]) VALUES (2, N'Region', 1, CAST(N'2015-06-25 23:15:50.140' AS DateTime), 1, NULL, NULL)
-GO
-INSERT [dbo].[RuleType] ([RuleTypeID], [Name], [CreateByUserID], [CreateDate], [Enabled], [ModifyByUserID], [ModifyDate]) VALUES (3, N'Quantity', 1, CAST(N'2015-06-25 23:15:50.140' AS DateTime), 1, NULL, NULL)
-GO
-INSERT [dbo].[RuleType] ([RuleTypeID], [Name], [CreateByUserID], [CreateDate], [Enabled], [ModifyByUserID], [ModifyDate]) VALUES (4, N'Odometer', 1, CAST(N'2015-06-25 23:15:50.140' AS DateTime), 1, NULL, NULL)
-GO
-SET IDENTITY_INSERT [dbo].[RuleType] OFF
 GO
